fd95cd694ebe47fe78dc23a1e1962a7b8ada9423
Giulian De Carlo <giuliandecarlo01@gmail.com>
2022-12-01 10:52:37 +0100

    Day 1: part 1,part2

---
+++ day1/day1.py
@@ -0,0 +1,27 @@
+
+#---------[PART 1]---------
+
+f=open('file.txt')
+list_elf=[]
+sumCal=0
+
+while True:
+    line=f.readline()
+    if not line:
+        break
+    if (line=="\n"):
+        list_elf.append(sumCal)
+        sumCal=0
+    else:
+        sumCal=sumCal+int(line)
+
+maxL=max(list_elf)
+print("Max: "+str(maxL))
+
+#---------[PART 2]---------
+
+list_elf.sort(reverse=True)
+sum=0
+for elf in list_elf[:3]:
+    sum=sum+elf
+print("Sum: "+str(sum))
