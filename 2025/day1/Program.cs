bdd40ff6565c96b0e65fba42cf6045e312541bc3
Giulian De Carlo <giuliandecarlo01@gmail.com>
2025-12-01 12:20:51 +0100

    Add day 1

---
+++ 2025/day1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        P1();
+        P2();
+    }
+
+    static void P1()
+    {
+        string[] lines = File.ReadAllLines("day1.txt");
+        int dial = 50;
+        int counter = 0;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i];
+            int number = int.Parse(line.Substring(1));
+            if (line[0] == 'L')
+                dial -= number;
+            else
+                dial += number;
+
+            dial = (dial % 100 + 100) % 100;
+            if (dial == 0) counter++;
+        }
+        Console.WriteLine("Part 1: " + counter);
+    }
+
+    static void P2()
+    {
+        string[] lines = File.ReadAllLines("day1.txt");
+        int dial = 50;
+        int counter = 0;
+
+        foreach (string line in lines)
+        {
+            int distance = int.Parse(line.Substring(1));
+
+            for (int i = 0; i < distance; i++)
+            {
+                if (line[0] == 'L')
+                {
+                    dial--;
+                    if (dial == -1) dial = 99;
+                }
+                else
+                {
+                    dial++;
+                    if (dial == 100) dial = 0;
+                }
+
+                if (dial == 0)
+                    counter++;
+            }
+        }
+        Console.WriteLine("Part 2: " + counter);
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Advent-of-Code
-My Advent of Code solutions, in Python.
+My Advent of Code solutions, in Python and C#.
 
 [Advent of Code](https://adventofcode.com) is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like.
